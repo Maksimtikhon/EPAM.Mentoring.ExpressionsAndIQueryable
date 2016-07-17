c30bba6b429cc700c17139198032e88c2126c032
Maksimtikhon <tikhon.maxim@gmail.com>
2016-07-17 23:47:45 +0300

    Added expression transformator

---
+++ EPAM.Mentoring.ExpressionsAndIQueryable/Task1.ExpressionTransformator/ExpressionTransformator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Task1.ExpressionTransformator
+{
+	public class ExpressionTransformator : ExpressionVisitor
+	{
+		public Dictionary<string, int> _paramDictionary = new Dictionary<string, int>();
+
+		public T ReplaceAddAndSubstract<T>(T expression) where T : Expression
+		{
+			return VisitAndConvert(expression, "");
+		}
+
+		public Expression ReplaceParameters(Expression expression, Dictionary<string, int> paramDictionary)
+		{
+			if (paramDictionary != null)
+			{
+				_paramDictionary = paramDictionary;
+			}
+
+			return Visit(expression);
+		}
+
+		protected override Expression VisitBinary(BinaryExpression node)
+		{
+			if (node.NodeType == ExpressionType.Add || node.NodeType == ExpressionType.Subtract)
+			{
+				var parameter = (node.Left.NodeType == ExpressionType.Parameter) ? (ParameterExpression)node.Left : null;
+				var constant = (node.Right.NodeType == ExpressionType.Constant) ? (ConstantExpression)node.Right : null;
+
+				if (parameter != null && constant != null && constant.Type == typeof(int) && (int)constant.Value == 1)
+				{
+					return node.NodeType == ExpressionType.Add ? Expression.Increment(parameter) : Expression.Decrement(parameter);
+				}
+			}
+
+			return base.VisitBinary(node);
+		}
+
+		protected override Expression VisitParameter(ParameterExpression node)
+		{
+			int value;
+			if (_paramDictionary.TryGetValue(node.Name, out value))
+			{
+				return Expression.Constant(value);
+			}
+
+			return base.VisitParameter(node);
+		}
+
+		protected override Expression VisitLambda<T>(Expression<T> node)
+		{
+			var parameters = node.Parameters.Where(p => _paramDictionary.Keys.Contains(p.Name) == false);
+
+			return Expression.Lambda(Visit(node.Body), parameters);
+		}
+	}
+}
---
+++ EPAM.Mentoring.ExpressionsAndIQueryable/Task1.ExpressionTransformator/ExpressionTransformatorTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace Task1.ExpressionTransformator
+{
+	[TestClass]
+	public class ExpressionTransformatorTest
+	{
+		[TestMethod]
+		public void AddToIncrementAndSubstractToDecrementTransformTest()
+		{
+			const int N = 1;
+			Expression<Func<int, int>> sourceExpression = (x) => (x + N) * (x - 5) * (x - 1) + (x + 1) * (x - N) * (x + 10);
+			var resultExpression = new ExpressionTransformator().ReplaceAddAndSubstract(sourceExpression);
+
+			var result = sourceExpression.Compile().Invoke(2);
+		  var resultAfterTransformation = resultExpression.Compile().Invoke(2);
+
+			Console.WriteLine("Source expression: {0} Result: {1}", sourceExpression, result);
+			Console.WriteLine("Result expression: {0} Result: {1}", resultExpression, resultAfterTransformation);
+
+			Assert.AreEqual(result, resultAfterTransformation);
+		}
+
+		[TestMethod]
+		public void ParameterToConstantTransformTest()
+		{
+			var dictionary = new Dictionary<string, int>()
+			{
+				{ "a", 0 },
+				{ "b", 3 },
+				{ "c", -4}
+			};
+
+			Expression<Func<int, int, int, int, int>> sourceExpression = (a, b, c, d) => a + b + c + d + 7;
+			var resultExpression = (Expression <Func<int, int>>)new ExpressionTransformator().ReplaceParameters(sourceExpression, dictionary);
+
+			var result = sourceExpression.Compile().Invoke(0, 3, -4, 1);
+			var resultAfterTransformation = resultExpression.Compile().Invoke(1);
+
+			Console.WriteLine("Source expression: {0} Result: {1}", sourceExpression, result);
+			Console.WriteLine("Result expression: {0} Result: {1}", resultExpression, resultAfterTransformation);
+
+		  Assert.AreEqual(result, resultAfterTransformation);
+		}
+	}
+}
